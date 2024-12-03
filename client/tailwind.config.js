8f8612f832d58934a926bfe91b063b17d20fedb9
oleksandrgrebinskij <roki000sasha@gmail.com>
2024-12-03 10:32:42 +0200

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,28 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     './src/**/*.{js,jsx,ts,tsx}',
+//     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('flowbite/plugin')
+//   ],
+// };
+//
+
+import flowbitePlugin from 'flowbite/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbitePlugin],
+};
